f73b5a92a0b519da60763e09ea6d98310688e176
Mark Glants <mark@glants.xyz>
2025-01-03 02:41:18 +0300

    fix: z-i changes

--- update.sh
+++ update.sh
@@ -4,15 +4,24 @@ set -e
 HERE="$(dirname "$(readlink -f "${0}")")"
 cd "$HERE"
 
-LISTLINK='https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv'
-NXDOMAINLINK='https://raw.githubusercontent.com/zapret-info/z-i/master/nxdomain.txt'
-curl -f --fail-early --compressed -o temp/list_orig.csv "$LISTLINK" || exit 1
-iconv -f cp1251 -t utf8 temp/list_orig.csv > temp/list.csv
-curl -f --fail-early --compressed -o temp/nxdomain.txt "$NXDOMAINLINK" || exit 1
+if [[ $SKIP_UPDATE_FROM_ZAPRET == true ]]; then
+    rm -f temp/list.csv temp/nxdomain.txt
+    echo -n > temp/list.csv
+    echo -n > temp/nxdomain.txt
+    exit 0
+fi
 
-LISTSIZE="$(curl -sI "$LISTLINK" | awk 'BEGIN {IGNORECASE=1;} /content-length/ {sub(/[ \t\r\n]+$/, "", $2); print $2}')"
-[[ "$LISTSIZE" != "$(stat -f '%z' temp/list_orig.csv)" ]] && echo "List 1 size differs" && exit 2
+LISTLINK='https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv.gz'
+curl -f --fail-early --compressed -o temp/list.csv.gz "$LISTLINK" || exit 1
+LISTSIZE="$(curl -sI "$LISTLINK"| awk 'BEGIN {IGNORECASE=1;} /content-length/ {sub(/[ \t\r\n]+$/, "", $2); print $2}')"
+[[ "$LISTSIZE" != "$(stat -c '%s' temp/list.csv.gz)" ]] && echo "List 1 size differs" && exit 2
+gunzip -fd temp/list.csv.gz || exit 3
+iconv -f cp1251 -t utf8 temp/list.csv | sponge temp/list.csv
+
+
+NXDOMAINLINK='https://raw.githubusercontent.com/zapret-info/z-i/master/nxdomain.txt'
+curl -f --fail-early --compressed -o temp/nxdomain.txt "$NXDOMAINLINK" || exit 1
 LISTSIZE="$(curl -sI "$NXDOMAINLINK" | awk 'BEGIN {IGNORECASE=1;} /content-length/ {sub(/[ \t\r\n]+$/, "", $2); print $2}')"
-[[ "$LISTSIZE" != "$(stat -f '%z' temp/nxdomain.txt)" ]] && echo "List 2 size differs" && exit 2
+[[ "$LISTSIZE" != "$(stat -c '%s' temp/nxdomain.txt)" ]] && echo "List 2 size differs" && exit 2
 
 exit 0
